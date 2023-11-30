29ac184bb16f27363223e90c7cdcca8a11328285
beknur.kettesh@gmail.com <beknur.kettesh@gmai.com>
2023-11-30 19:39:42 +0600

    assets card

--- QazaqDebate-Mobile/src/navigation/TabNavigator.js
+++ QazaqDebate-Mobile/src/navigation/TabNavigator.js
@@ -1,7 +1,8 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
-import { createStackNavigator } from '@react-navigation/stack'
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+import { createStackNavigator } from "@react-navigation/stack";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import Home from "../screens/HomeScreens/Home";
 
 const Stack = createStackNavigator();
 const Tab = createBottomTabNavigator();
@@ -9,14 +10,14 @@ const Tab = createBottomTabNavigator();
 const TabNavigator = () => {
   return (
     <Tab.Navigator>
-        <Tab.Screen name="Home" component={HomeTabs} />
-        <Tab.Screen name="Tools" component={Tools} />
-        <Tab.Screen name="Services" component={Services} />
-        <Tab.Screen name='Profile' component={Profile} />
+      <Tab.Screen name="Home" component={Home} />
+      <Tab.Screen name="Resources" component={Home} />
+      <Tab.Screen name="Tools" component={Home} />
+      <Tab.Screen name="Profile" component={Home} />
     </Tab.Navigator>
-  )
-}
+  );
+};
 
-export default TabNavigator
+export default TabNavigator;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});

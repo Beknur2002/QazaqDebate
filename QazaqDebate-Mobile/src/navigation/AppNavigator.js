881a9585b796cea2a0081d5a4b33551a6951a896
Beknur Kettesh <beknur.kettesh@gmail.com>
2024-01-08 14:35:54 +0600

    some details

--- QazaqDebate-Mobile/src/navigation/AppNavigator.js
+++ QazaqDebate-Mobile/src/navigation/AppNavigator.js
@@ -1,29 +1,40 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React, { useState } from 'react'
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
-import 'react-native-gesture-handler';
-import { createStackNavigator } from '@react-navigation/stack';
-import { NavigationContainer } from '@react-navigation/native';
-import TabNavigator from './TabNavigator';
-import AuthStack from './AuthStack';
-import TimerScreen from '../screens/DebateKeeperScreens/TimerScreen';
+import { StyleSheet, Text, View } from "react-native";
+import React, { useState } from "react";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import "react-native-gesture-handler";
+import { createStackNavigator } from "@react-navigation/stack";
+import { NavigationContainer } from "@react-navigation/native";
+import TabNavigator from "./TabNavigator";
+import AuthStack from "./AuthStack";
+import TimerScreen from "../screens/DebateKeeperScreens/TimerScreen";
 
 const Tab = createBottomTabNavigator();
-const Stack = createStackNavigator()
+const Stack = createStackNavigator();
 
 const AppNavigator = () => {
-const [signed, setSigned] = useState(true)
-    return (
-        <NavigationContainer>
-            {signed ? 
-            <Stack.Navigator >
-                <Stack.Screen name="Tab" component={TabNavigator} options={{headerShown: false}}  />
-                <Stack.Screen name="TimerScreen" component={TimerScreen} options={({ route }) => ({ title: route.params })}/>
-            </Stack.Navigator> : <AuthStack/>}
-        </NavigationContainer>
-    )
-}
+  const [signed, setSigned] = useState(true);
+  return (
+    <NavigationContainer>
+      {signed ? (
+        <Stack.Navigator>
+          <Stack.Screen
+            name="Tab"
+            component={TabNavigator}
+            options={{ headerShown: false }}
+          />
+          <Stack.Screen
+            name="TimerScreen"
+            component={TimerScreen}
+            options={({ route }) => ({ title: route.params })}
+          />
+        </Stack.Navigator>
+      ) : (
+        <AuthStack />
+      )}
+    </NavigationContainer>
+  );
+};
 
-export default AppNavigator
+export default AppNavigator;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
